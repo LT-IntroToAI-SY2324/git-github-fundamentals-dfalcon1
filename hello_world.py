9313a6c078886417f5af29fd985d3ae48bc02bf6
Daniel Falcon <dfalcon1.edu>
2023-08-29 10:37:41 -0500

    Added a bunch stuff about basics

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
 print("hello world")
+name = "Daniel Falcon"
+print("I am", name , "I am pleased to meet you")
+x= 6
+y= 10
+def sum(x,y):
+    sum = x + y
+    return sum
+print("my favorite number is" , sum(x,y))
+
